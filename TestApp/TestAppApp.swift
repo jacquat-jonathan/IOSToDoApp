db15630f47dabf04e8cd677bc4a3d996f37e328c
Jacquat Jonathan <jacquat.jonathan@gmail.com>
2024-10-14 15:37:14 +0200

    Initial Commit

---
+++ TestApp/TestAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestAppApp.swift
+//  TestApp
+//
+//  Created by Jonathan Jacquat on 14.10.2024.
+//
+
+import SwiftUI
+
+@main
+struct TestAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
