a91400a511fe2d88a72953f891581d99b5d6d3e4
Jacquat Jonathan <jacquat.jonathan@gmail.com>
2024-10-26 15:41:57 +0200

    updated calendar view (under construction)

--- ToDoApp/Views/CalendarView.swift
+++ ToDoApp/Views/CalendarView.swift
@@ -9,8 +9,18 @@ import SwiftUI
 
 struct CalendarView: View {
     var body: some View {
-        Text("This view will be used to display the calendar (like a real calendar). Usefull to see task far in the future. Use IOS Calendar app for example")
-            .padding()
+        VStack {
+            HStack {
+                Image(systemName: "xmark.circle.fill")
+                    .font(.system(size: 25))
+                Text("Under construction")
+                Image(systemName: "xmark.circle.fill")
+                    .font(.system(size: 25))
+            }
+            .padding(.bottom)
+            Text("This view will be used to display the calendar (like a real calendar). Usefull to see task far in the future. Use IOS Calendar app for example")
+        }
+        .padding()
     }
 }
 
